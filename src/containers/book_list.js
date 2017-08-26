cc70bd922e3bc167567df90888d8c99de6998a45
Vikram <vikram.prasad@mountblue.io>
2017-08-26 15:47:07 +0530

    Action creator made and action object is passed to the reducers

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { selectBook } from '../actions/index';
 
 class BookList extends Component {
 
@@ -7,7 +10,8 @@ class BookList extends Component {
     return this.props.books.map(book =>{
       return (
         <li className="list-group-item"
-          key={book.title}>
+          key={book.title}
+          onClick={()=>{this.props.selectBook(book)}}>
           {book.title}
         </li>
       );
@@ -28,4 +32,7 @@ const mapStateToProps = (state)=>{
   }
 }
 
-export default connect(mapStateToProps)(BookList);
+const mapDispatchToProps = (dispatch)=>{
+  return bindActionCreators({ selectBook : selectBook}, dispatch);
+}
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
